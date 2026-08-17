ce331904b67689fb12a3c94a91cc95a09e507e3d
robmorris74 <rob.morris@egc-inc.com>
2026-08-16 23:02:56 -0500

    Change Firebase API key in configuration
    Updated the Firebase API key for the BBCOR project.

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,8 @@
 // BBCOR Property Command Center - Firebase configuration
 // Configured for the BBCOR COMMAND CENTER Firebase project.
+
 window.BBCOR_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAaZp8J4VyFfQvejSik89PUGlmgGLJ81aI",
+  apiKey: "AIzaSyAaZp8J4VvFfOveiSjk89PUGlmgGlJ81aI",
   authDomain: "bbcor-command-cent.firebaseapp.com",
   databaseURL: "https://bbcor-command-cent-default-rtdb.firebaseio.com",
   projectId: "bbcor-command-cent",
